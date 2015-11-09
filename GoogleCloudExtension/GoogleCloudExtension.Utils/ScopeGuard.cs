be5df7dc44eaafbb25d10cf3347122f85c70dac2
Ivan Naranjo <ivann@google.com>
2015-11-09 10:41:17 +0000

    Utils project.

---
+++ GoogleCloudExtension/GoogleCloudExtension.Utils/ScopeGuard.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2015 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using System;
+
+namespace GoogleCloudExtension.Utils
+{
+    public class ScopeGuard : IDisposable
+    {
+        private readonly Action _action;
+        public ScopeGuard(Action action)
+        {
+            _action = action;
+        }
+
+        public void Dispose()
+        {
+            _action();
+        }
+    }
+}
