5e1dba49578dcaf161aa8ef3c79311aace8d10e6
Ivan Naranjo <ivann@google.com>
2015-12-22 12:29:54 +0000

    Adding the icons for the nodes.

--- GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/AppEngine/AppEngineRootViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/AppEngine/AppEngineRootViewModel.cs
@@ -4,15 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace GoogleCloudExtension.CloudExplorerSources.AppEngine
 {
     internal class AppEngineRootViewModel : TreeHierarchy
     {
+        static readonly Lazy<BitmapImage> s_icon = new Lazy<BitmapImage>(LoadIcon);
+
+        private static BitmapImage LoadIcon()
+        {
+            var uri = new Uri("pack://application:,,,/GoogleCloudExtension;component/CloudExplorerSources/AppEngine/Resources/app_engine.png");
+            return new BitmapImage(uri);
+        }
+
         public AppEngineRootViewModel()
         {
             Content = "AppEngine";
-            // TODO: Set the icon for AppEngine.
+            Icon = s_icon.Value;
         }
     }
 }
