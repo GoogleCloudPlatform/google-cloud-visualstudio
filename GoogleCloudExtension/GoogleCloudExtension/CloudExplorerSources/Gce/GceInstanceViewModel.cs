6a1fe00ebb62b81812c55c16805610670f4403f6
Ivan Naranjo <ivann@google.com>
2016-02-03 00:29:58 +0000

    Adding icons for zone and instance.

--- GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/Gce/GceInstanceViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/Gce/GceInstanceViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using GoogleCloudExtension.CloudExplorer;
+using System.Windows.Media;
+using GoogleCloudExtension.Utils;
 
 namespace GoogleCloudExtension.CloudExplorerSources.Gce
 {
     internal class GceInstanceViewModel : TreeLeaf, ICloudExplorerItemSource
     {
+        private const string IconResourcePath = "CloudExplorerSources/AppEngine/Resources/ic_web.png";
+        private static readonly Lazy<ImageSource> s_instanceIcon = new Lazy<ImageSource>(() => ResourceUtils.LoadResource(IconResourcePath));
+
         private GceInstance _instance;
 
         public GceInstanceViewModel(GceInstance instance)
         {
             Content = instance.Name;
+            Icon = s_instanceIcon.Value;
             _instance = instance;
         }
 
--- GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/Gce/ZoneViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudExplorerSources/Gce/ZoneViewModel.cs
@@ -1,14 +1,21 @@
 ﻿using GoogleCloudExtension.CloudExplorer;
+using GoogleCloudExtension.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Media;
 
 namespace GoogleCloudExtension.CloudExplorerSources.Gce
 {
     public class ZoneViewModel : TreeHierarchy
     {
+        private const string IconResourcePath = "CloudExplorerSources/AppEngine/Resources/ic_view_module.png";
+        private static readonly Lazy<ImageSource> s_zoneIcon = new Lazy<ImageSource>(() => ResourceUtils.LoadResource(IconResourcePath));
+
         public ZoneViewModel(string key, IEnumerable<GceInstance> instances)
         {
             Content = key;
+            Icon = s_zoneIcon.Value;
 
             var viewModels = instances.Select(x => new GceInstanceViewModel(x));
             foreach (var viewModel in viewModels)
