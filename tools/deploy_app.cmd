7802359745aa845aca7cd8668be94cc5b7cb9831
Ivan Naranjo <ivann@google.com>
2016-02-16 14:39:45 +0000

    Adding script to deploy and app given the publishsettings file.

---
+++ tools/deploy_app.cmd
@@ -0,0 +1,28 @@
+REM @echo off
+REM Deploy the given app using the given deployment profile.
+REM   %1, the path to the project.
+REM   %2, where to publish the app.
+REM   %3, the path to the publish settings.
+
+setlocal
+
+echo "Building and deploying the app from %1, using profile %2."
+msbuild "%~1" ^
+  /p:Configuration=Release ^
+  /p:Platform=AnyCPU ^
+  /t:WebPublish ^
+  /p:WebPublishMethod=FileSystem ^
+  /p:DeleteExistingFiles=True ^
+  /p:publishUrl="%~2"
+
+set default_site="Default Web Site"
+set msdeploy_path="%ProgramFiles%\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+
+echo "Deploying using the profile %3."
+%msdeploy_path% ^
+  -verb:sync ^
+  -source:contentPath="%~2" ^
+  -dest:contentPath=%default_site%,publishSettings="%~3" ^
+  -allowUntrusted
+
+echo "Done."
