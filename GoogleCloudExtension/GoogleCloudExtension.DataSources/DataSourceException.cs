50475505757f2e288b6b9cd33f5c88782edf8851
Ivan Naranjo <ivann@google.com>
2016-03-31 13:22:57 +0100

    Proper implementation of the exception.

--- GoogleCloudExtension/GoogleCloudExtension.DataSources/DataSourceException.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/DataSourceException.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2015 Google Inc. All Rights Reserved.
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
 // Licensed under the Apache License Version 2.0.
 
 using System;
@@ -7,15 +7,24 @@ using System.Runtime.Serialization;
 namespace GoogleCloudExtension.DataSources
 {
     /// <summary>
-    /// Exception to be thrown from the data sources in this project.
+    /// Exception thrown when there's an issue in a data source.
     /// </summary>
+    [Serializable]
     public class DataSourceException : Exception
     {
+        public DataSourceException()
+        {
+        }
+
         public DataSourceException(string message) : base(message)
         {
         }
 
-        public DataSourceException(string message, Exception inner) : base(message, inner)
+        public DataSourceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DataSourceException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
