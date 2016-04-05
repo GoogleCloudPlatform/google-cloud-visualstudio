f49dc37d51f26fffa0511a3cef7275b2e3f4d804
Ivan Naranjo <ivann@google.com>
2016-04-05 15:45:04 +0100

    Adding credentials model for UI.

---
+++ GoogleCloudExtension/GoogleCloudExtension/CredentialsManagement/Models/UserCredentials.cs
@@ -0,0 +1,35 @@
+﻿using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GoogleCloudExtension.CredentialsManagement.Models
+{
+    public class UserCredentials : Model
+    {
+        private AsyncPropertyValue<ImageSource> _profilePictureAsync;
+        private AsyncPropertyValue<string> _nameAsync;
+        private string _account;
+
+        public AsyncPropertyValue<ImageSource> ProfilePictureAsync
+        {
+            get { return _profilePictureAsync; }
+            set { SetValueAndRaise(ref _profilePictureAsync, value); }
+        }
+
+        public AsyncPropertyValue<string> NameAsync
+        {
+            get { return _nameAsync; }
+            set { SetValueAndRaise(ref _nameAsync, value); }
+        }
+
+        public string Account
+        {
+            get { return _account; }
+            set { SetValueAndRaise(ref _account, value); }
+        }
+    }
+}
