608f91467007b465c9f1daba4ee9e1cde776ff00
Ivan Naranjo <ivann@google.com>
2016-04-18 11:00:35 +0100

    Comments, cleanup for user prompts.

--- GoogleCloudExtension/GoogleCloudExtension/Utils/UserPromptUtils.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Utils/UserPromptUtils.cs
@@ -12,6 +12,15 @@ namespace GoogleCloudExtension.Utils
 {
     public static class UserPromptUtils
     {
+        private const int IDYES = 6;
+        private const int IDNO = 7;
+
+        /// <summary>
+        /// Show a message dialog with a Yes and No button to the user.
+        /// </summary>
+        /// <param name="message">The message for the dialog.</param>
+        /// <param name="title">The title for the dialog.</param>
+        /// <returns>Returns true if the user pressed the YES button.</returns>
         public static bool YesNoPrompt(string message, string title)
         {
             var result = VsShellUtilities.ShowMessageBox(
@@ -21,9 +30,14 @@ namespace GoogleCloudExtension.Utils
                     OLEMSGICON.OLEMSGICON_QUERY,
                     OLEMSGBUTTON.OLEMSGBUTTON_YESNO,
                     OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_SECOND);
-            return result == ButtonIds.IDYES;
+            return result == IDYES;
         }
 
+        /// <summary>
+        /// Shows a message dialog to the user with an Ok button.
+        /// </summary>
+        /// <param name="message">The message for the dialog.</param>
+        /// <param name="title">The title for the dialog.</param>
         public static void OkPrompt(string message, string title)
         {
             VsShellUtilities.ShowMessageBox(
@@ -35,6 +49,11 @@ namespace GoogleCloudExtension.Utils
                     OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
 
+        /// <summary>
+        /// Shows an error message dialog to the user, with an Ok button.
+        /// </summary>
+        /// <param name="message">The message for the dialog.</param>
+        /// <param name="title">The title for the dialog.</param>
         public static void ErrorPrompt(string message, string title)
         {
             VsShellUtilities.ShowMessageBox(
