8d8d60c6079aeb30e95dde4efec2377b2a65e4a4
Ivan Naranjo <ivann@google.com>
2016-04-29 15:00:35 +0100

    Adding the oauth library.

---
+++ GoogleCloudExtension/GoogleCloudExtension.OAuth/OAuthException.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace GoogleCloudExtension.OAuth
+{
+    /// <summary>
+    /// Exception thrown when encountering issues with oauth servers.
+    /// </summary>
+    [Serializable]
+    public class OAuthException : Exception
+    {
+        public OAuthException()
+        {
+        }
+
+        public OAuthException(string message) : base(message)
+        {
+        }
+
+        public OAuthException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OAuthException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension.OAuth/OauthCredentials.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
+
+namespace GoogleCloudExtension.OAuth
+{
+    /// <summary>
+    /// Contains the OAUTH credentials to use for OAUTH authentication.
+    /// </summary>
+    public class OAuthCredentials
+    {
+        public string ClientId { get; }
+
+        public string ClientSecret { get; }
+
+        public OAuthCredentials(string clientId, string clientSecret)
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+    }
+}
