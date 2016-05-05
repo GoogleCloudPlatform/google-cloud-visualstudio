5ee27c983596a7995668a824a134ab362d8d112c
Ivan Naranjo <ivann@google.com>
2016-05-05 11:10:17 +0100

    Drop of the q2_tools.

---
+++ tools/ensure_license.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import fnmatch
+import os
+
+# Parser for the arguments for the program.
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--directory',
+                    help='The directory where the source lives.',
+                    required=True)
+parser.add_argument('-l', '--license',
+                    help='The license to use.',
+                    required=True)
+parser.add_argument('-f', '--force',
+                    help='Update license even if one is found.',
+                    required=False,
+                    dest='force',
+                    action='store_true')
+parser.add_argument('-v', '--verbose',
+                    help='Show verbose output.',
+                    required=False,
+                    dest='verbose',
+                    action='store_true')
+
+
+# The bom mark at the begining of the file.
+BOM_MARK = '\xef\xbb\xbf'
+COMMENT_START = '//'
+
+
+def load_all_files(dir):
+    """Returns all of the csharp source files."""
+    result = []
+    for root, dirnames, filenames in os.walk(dir):
+        if 'obj\\' not in root:
+            for name in fnmatch.filter(filenames, '*.cs'):
+                result.append(os.path.join(root, name))
+    return result
+
+
+class SourceFile(object):
+    def __init__(self, path):
+        with open(path, 'r') as src:
+            self.contents = src.readlines()
+        self.path = path
+        self.has_bom = False
+        self.has_license = False
+        self._process_contents()
+
+    def _process_contents(self):
+        # Removes the BOM mark at the begining of the file. They will
+        # be added later when serializing the file.
+        if self.contents[0].startswith(BOM_MARK):
+            self.has_bom = True
+            self.contents[0] = self.contents[0][len(BOM_MARK):]
+
+        # Assume that the comment at the begining of the file are
+        # licenses.
+        first_line = self.contents[0]
+        if first_line.startswith(COMMENT_START):
+            self.has_license = True
+        else:
+            return
+        for i in range(1, len(self.contents)):
+            line = self.contents[i]
+            if not line.startswith(COMMENT_START):
+                self.license_end = i
+                return
+
+    def replace_license(self, new_license, force):
+        if self.has_license and not force:
+            print ("Skipping file %s, already has license" % self.path)
+            return
+        if self.has_license:
+            source_code = self.contents[self.license_end:]
+        else:
+            source_code = self.contents
+
+        # Ensure that there is an empty line between the license text
+        # and the first line of text.
+        if source_code[0] != "\n":
+            source_code = ["\n"] + source_code
+
+        self.contents = new_license + source_code
+
+    def print(self):
+        print ("Path: %s" % self.path)
+        print ("Has Bom: %s" % self.has_bom)
+        if self.has_license:
+            print ("License:")
+            for i in range(0, self.license_end):
+                print ("  %s" % self.contents[i], end="")
+            print ("End license")
+        else:
+            print ("No license")
+        print ("")
+
+    def update_file(self):
+        if params.verbose:
+            print ("Updating file: %s" % self.path)
+        with open(self.path, 'wt') as dest:
+            if self.has_bom:
+                dest.write(BOM_MARK)
+            for line in self.contents:
+                dest.write(line)
+
+
+def main():
+    files = load_all_files(params.directory)
+    with open(params.license, 'r') as src:
+        new_license = src.readlines()
+    for file in files:
+        source_file = SourceFile(file)
+        if params.verbose:
+            source_file.print()
+        source_file.replace_license(new_license, force=params.force)
+        source_file.update_file()
+
+
+if __name__ == "__main__":
+    params = parser.parse_args()
+    main()
