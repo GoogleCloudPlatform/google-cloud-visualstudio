d7cd47a2ceb027cd79af4f7030eb27d6438bd3b9
Ivan Naranjo <ivann@google.com>
2016-05-19 13:47:46 +0100

    Adding the ASP.NET 4.x MVC template for Google Cloud.
    This PR adds the initial ASP.NET 4.x MVC template for Google Cloud so
    start ASP.NET applications for GCE. More customizations to come later.

---
+++ tools/publish_template.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env bash
+# Releases the given template into the extension.
+#   $1, the directory with the template to release.
+#   $2, the sub-directory where to publish the template.
+
+# Ensure we fail on errors.
+set -eu
+
+# Get workspace location.
+readonly workspace=$(dirname $0)/..
+
+# Expand parameters.
+readonly template_dir=${1:-""}
+readonly dest_dir=${2:-""}
+
+# Check mandatory parameters.
+if [ -z ${template_dir} ]; then
+    echo "Need to specify the project and template location."
+    exit -1
+fi
+
+# Gather information about the source and targe directories.
+readonly template_name="$(basename $1)"
+readonly templates_dir="${workspace}/GoogleCloudExtension/ProjectTemplates"
+readonly extension_dir="${workspace}/GoogleCloudExtension/GoogleCloudExtension"
+
+# Determine the extension templates directory to use.
+if [ -n "${dest_dir}" ]; then
+    readonly extension_templates="${extension_dir}/ProjectTemplates/${dest_dir}"
+else
+    readonly extension_templates="${extension_dir}/ProjectTemplates"
+fi
+
+# The final path for the template zip.
+readonly released_template_zip="${extension_templates}/${template_name}.zip"
+
+# Ensure the extension's ProjectTemplate exists.
+echo "Creating the ProjectTemplates directory: ${extension_templates}"
+mkdir -p "${extension_templates}"
+
+# Delete the existing .zip file and compress again. 
+echo "Deleting existing zip: ${released_template_zip}"
+rm "${released_template_zip}" || true
+
+# Compress and release the extention.
+echo "Compressing template: ${template_name}"
+${workspace}/tools/build_zip.py -o "${released_template_zip}" -d "$1"
+
+echo "Done."
