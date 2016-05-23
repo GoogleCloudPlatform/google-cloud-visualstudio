19d3c5d0e9a534a909c47e15a20b9c0419918fd7
Ivan Naranjo <ivann@google.com>
2016-05-23 14:48:49 +0100

    Adding the Web API template to the project.

---
+++ tools/publish_all_templates.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+# Publishes all of the project templates in the project.
+
+# Ensure we fail on errors.
+set -eu
+
+# Get workspace location.
+readonly workspace=$(dirname $0)/..
+
+# List all project templates and publishe them.
+for p in $(find "${workspace}/GoogleCloudExtension/ProjectTemplates" -mindepth 1 -maxdepth 1); do
+    echo "Publishing project: $(basename ${p})"
+    ${workspace}/tools/publish_template.sh "${p}" "Google Cloud Platform"
+done
+
+echo Done.
