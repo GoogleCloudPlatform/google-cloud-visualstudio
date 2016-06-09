d4324144a575d8a90d7806138218e387a6570ca7
Ivan Naranjo <ivann@google.com>
2016-06-09 11:27:25 +0100

    Adding comments.

--- GoogleCloudExtension/GoogleCloudExtension.DataSources/FirewallPort.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/FirewallPort.cs
@@ -1,15 +1,39 @@
 ﻿namespace GoogleCloudExtension.DataSources
 {
+    /// <summary>
+    /// The protocols for the port.
+    /// </summary>
+    public enum PortProtocol
+    {
+        Tcp,
+        Udp,
+    }
+
+    /// <summary>
+    /// This class represents a TCP port that needs to be opened in the firewall.
+    /// </summary>
     public class FirewallPort
     {
+        /// <summary>
+        /// The name to use for the port rule and port tags.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// The port number.
+        /// </summary>
         public int Port { get; }
 
-        public FirewallPort(string name, int port)
+        /// <summary>
+        /// The port protocol.
+        /// </summary>
+        public PortProtocol Protocol { get; }
+
+        public FirewallPort(string name, int port, PortProtocol protocol = PortProtocol.Tcp)
         {
             Name = name;
             Port = port;
+            Protocol = protocol;
         }
     }
 }
