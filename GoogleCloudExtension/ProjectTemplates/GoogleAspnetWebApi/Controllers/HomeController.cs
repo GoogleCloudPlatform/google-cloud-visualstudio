2d3acb58a096a32cda8a016838bbf8569ff68be8
remi Taylor <remi@remitaylor.com>
2016-07-20 09:38:40 -0700

    Update ASP.NET Web API template to use MySQL

--- GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Controllers/HomeController.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using MySql.Data.MySqlClient;
+using $safeprojectname$.Models;
 
 namespace $safeprojectname$.Controllers
 {
@@ -11,6 +14,19 @@ namespace $safeprojectname$.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            var dbContext = HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+
+            try
+            {
+                dbContext.Database.Connection.Open();
+                ViewBag.DatabaseError = false;
+            }
+            catch (MySqlException ex)
+            {
+                ViewBag.DatabaseError = true;
+                ViewBag.DatabaseErrorMessage = $"{ex.GetType().Name} - {ex.Message}";
+            }
 
             return View();
         }
--- GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Models/IdentityModels.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
 ﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace $safeprojectname$.Models
 {
