606ea2ced5f59801770e0684c39057e001ee5b4b
Ivan Naranjo <ivann@google.com>
2016-08-24 14:06:19 +0000

    Initial drop of the dialog.

---
+++ GoogleCloudExtension/GoogleCloudExtension/PublishDialog/PublishDialogWindow.cs
@@ -0,0 +1,26 @@
+﻿using GoogleCloudExtension.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.PublishDialog
+{
+    public class PublishDialogWindow : CommonDialogWindowBase
+    {
+        private PublishDialogWindowViewModel ViewModel { get; }
+
+        private PublishDialogWindow() : base("Publish Application", 500, 400)
+        {
+            ViewModel = new PublishDialogWindowViewModel(this);
+            Content = new PublishDialogWindowContent { DataContext = ViewModel };
+        }
+
+        public static void PromptUser()
+        {
+            var dialog = new PublishDialogWindow();
+            dialog.ShowModal();
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/PublishDialog/PublishDialogWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.PublishDialog
+{
+    public class PublishDialogWindowViewModel : ViewModelBase
+    {
+        private readonly PublishDialogWindow _owner;
+
+        public PublishDialogWindowViewModel(PublishDialogWindow owner)
+        {
+            _owner = owner;
+        }
+    }
+}
