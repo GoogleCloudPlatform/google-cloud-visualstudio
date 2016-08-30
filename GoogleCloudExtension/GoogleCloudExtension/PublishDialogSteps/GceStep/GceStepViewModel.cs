4d8bcb7204605c5f0c89daf164b0e58e00b843b6
Ivan Naranjo <ivann@google.com>
2016-08-30 16:07:05 +0000

    Initial implementation of the loading of instances and credentials for the Gce step.

--- GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/GceStep/GceStepViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/GceStep/GceStepViewModel.cs
@@ -1,4 +1,9 @@
 ﻿using EnvDTE;
+using Google.Apis.Compute.v1.Data;
+using GoogleCloudExtension.Accounts;
+using GoogleCloudExtension.DataSources;
+using GoogleCloudExtension.GCloud;
+using GoogleCloudExtension.ManageWindowsCredentials;
 using GoogleCloudExtension.PublishDialog;
 using GoogleCloudExtension.Utils;
 using System;
@@ -7,22 +12,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace GoogleCloudExtension.PublishDialogSteps.GceStep
 {
     public class GceStepViewModel : ViewModelBase, IPublishDialogStep
     {
         private readonly GceStepContent _content;
-        private readonly Project _currentProject;
+        private EnvDTE.Project _currentProject;
+        private Instance _selectedInstance;
+        private IEnumerable<WindowsInstanceCredentials> _credentials;
+        private WindowsInstanceCredentials _selectedCredentials;
+
+        public AsyncPropertyValue<IEnumerable<Instance>> Instances { get; }
+
+        public Instance SelectedInstance
+        {
+            get { return _selectedInstance; }
+            set
+            {
+                SetValueAndRaise(ref _selectedInstance, value);
+                UpdateCredentials();
+                ManageCredentialsCommand.CanExecuteCommand = value != null;
+            }
+        }
+
+        public IEnumerable<WindowsInstanceCredentials> Credentials
+        {
+            get { return _credentials; }
+            private set { SetValueAndRaise(ref _credentials, value); }
+        }
+
+        public WindowsInstanceCredentials SelectedCredentials
+        {
+            get { return _selectedCredentials; }
+            set
+            {
+                SetValueAndRaise(ref _selectedCredentials, value);
+                RaisePropertyChanged(nameof(HasSelectedCredentials));
+            }
+        }
+
+        public bool HasSelectedCredentials => SelectedCredentials != null;
+
+        public WeakCommand ManageCredentialsCommand { get; }
 
         private GceStepViewModel(GceStepContent content)
         {
             _content = content;
 
-            _currentProject = SolutionHelper.CurrentSolution.StartupProject;
+            Instances = AsyncPropertyValueUtils.CreateAsyncProperty(GetAllAspNetInstances());
+
+            ManageCredentialsCommand = new WeakCommand(OnManageCredentialsCommand, canExecuteCommand: false);
         }
 
-        #region IPublishDialogTarget
+        private void OnManageCredentialsCommand()
+        {
+            ManageWindowsCredentialsWindow.PromptUser(SelectedInstance);
+            UpdateCredentials();
+        }
+
+        private async Task<IEnumerable<Instance>> GetAllAspNetInstances()
+        {
+            var dataSource = new GceDataSource(
+                CredentialsStore.Default.CurrentProjectId,
+                CredentialsStore.Default.CurrentGoogleCredential,
+                GoogleCloudExtensionPackage.ApplicationName);
+            var instances = await dataSource.GetInstanceListAsync();
+            return instances.Where(x => x.IsRunning() && x.IsAspnetInstance());
+        }
+
+        #region IPublishDialogStep
 
         bool IPublishDialogStep.CanGoNext => false;
 
@@ -41,7 +101,9 @@ namespace GoogleCloudExtension.PublishDialogSteps.GceStep
         }
 
         void IPublishDialogStep.OnPushedToDialog(IPublishDialog dialog)
-        { }
+        {
+            _currentProject = dialog.Project;
+        }
 
         #endregion
 
@@ -54,5 +116,17 @@ namespace GoogleCloudExtension.PublishDialogSteps.GceStep
             return viewModel;
         }
 
+        private void UpdateCredentials()
+        {
+            if (SelectedInstance == null)
+            {
+                Credentials = Enumerable.Empty<WindowsInstanceCredentials>();
+            }
+            else
+            {
+                Credentials = WindowsCredentialsStore.Default.GetCredentialsForInstance(SelectedInstance);
+            }
+            SelectedCredentials = Credentials.FirstOrDefault();
+        }
     }
 }
