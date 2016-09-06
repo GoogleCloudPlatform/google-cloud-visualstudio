ed3914765fcdb1878e4a60e37c7223faaca56bc7
Ivan Naranjo <ivann@google.com>
2016-09-06 14:01:19 +0000

    Organizing usings.

--- GoogleCloudExtension/GoogleCloudExtension.Deployment/NetCorePublishResult.cs
+++ GoogleCloudExtension/GoogleCloudExtension.Deployment/NetCorePublishResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GoogleCloudExtension.Deployment
+﻿namespace GoogleCloudExtension.Deployment
 {
     public class NetCorePublishResult
     {
--- GoogleCloudExtension/GoogleCloudExtension/Utils/ProgressBarHelper.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Utils/ProgressBarHelper.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.Shell.Interop;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GoogleCloudExtension.Utils
 {
