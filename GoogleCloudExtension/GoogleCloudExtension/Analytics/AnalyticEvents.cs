22731e71586e8d8554a70bf55d67be18f816addd
Ivan Naranjo <ivann@google.com>
2016-09-15 17:13:30 +0100

    Initial drop (partial) of the list of events.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Analytics/AnalyticEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.Analytics
+{
+    internal static class AnalyticEvents
+    {
+        public const string ExtensionEventType = "vsextension";
+
+        public const string NewInstallEventName = "newInstall";
+        public const string UpgradeEventName = "upgrade";
+        public const string NewLoginEventName = "newLogin";
+        public const string CloudExplorerInteractionEventName = "cloudExplorerInteraction";
+        public const string GceAppDeploymentEventName = "gceAppDeployment";
+        public const string FlexAppDeploymentEventName = "flexAppDeployment";
+        public const string GceVMListLoadEventName = "gceVMsLoad";
+        public const string GcsBucketListLoadEventName = "gcsBucketsLoad";
+        public const string CloudSQLInstancesListLoadEventName = "cloudSQLInstancesLoad";
+    }
+}
