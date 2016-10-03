587bbde9de83019d10b26c8628b51b505f1a2c91
Ivan Naranjo <ivann@google.com>
2016-10-03 12:48:56 +0100

    Review comments.

--- tools/resx2csv.py
+++ tools/resx2csv.py
@@ -4,6 +4,9 @@
 This script will take any .resx file and extract all of the strings to a .csv file
 easily importable into spread sheets.
 
+Example of usage:
+resx2csv.py --resx GoogleCloudExtension\Resources.resx -o strings.csv
+
 """
 
 from __future__ import print_function
@@ -35,10 +38,10 @@ def convert_to_csv(root, output):
     """Takes the root of the Xml file and converts it into a .csv file."""
 
     with open(output, 'wt') as out:
-        out.write("name,value,comment\n")
+        out.write('name,value,comment\n')
         for child in root.findall('data'):
             str = StringDefinition(child)
-            line = "\"{}\",\"{}\",\"{}\"\n".format(str.name, str.value, str.comment)
+            line = '\"{}\",\"{}\",\"{}\"\n'.format(str.name, str.value, str.comment)
             out.write(line)
 
 
@@ -48,7 +51,7 @@ def main(params):
     convert_to_csv(root, params.output)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     params = parser.parse_args()
     main(params)
 
