f8be5f0422e08c35c537f1b9b83a68b8298cc354
Ivan Naranjo <ivann@google.com>
2016-10-10 14:37:42 +0100

    Adding the initial error handler.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Utils/ErrorHandlerUtils.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.Utils
+{
+    public static class ErrorHandlerUtils
+    {
+        public static void HandleExceptions(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (AggregateException ex)
+            {
+                Debug.WriteLine($"Uncaught aggregate exception: {ex.Message}");
+                if (ErrorHandler.ContainsCriticalException(ex))
+                {
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Uncaught exception: {ex.Message}");
+                if (ErrorHandler.IsCriticalException(ex))
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
