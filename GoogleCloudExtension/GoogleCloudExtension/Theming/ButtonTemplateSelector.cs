e89fd9bdff245128b3915460c68a1711af8dc9ac
Ivan Naranjo <ivann@google.com>
2016-10-24 15:06:35 +0100

    Adding template selector to choose the right size of a button.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Theming/ButtonTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GoogleCloudExtension.Theming
+{
+    public class ButtonTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var buttonInfo = (DialogButtonInfo)item;
+            var element = (FrameworkElement)container;
+
+            if (buttonInfo.Caption.Length > 6)
+            {
+                return (DataTemplate)element.FindResource("DialogButtonTemplateWide");
+            }
+            else
+            {
+                return (DataTemplate)element.FindResource("DialogButtonTemplateStandard");
+            }
+        }
+    }
+}
