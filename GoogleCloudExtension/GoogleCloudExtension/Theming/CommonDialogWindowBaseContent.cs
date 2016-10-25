9e5c9a0ca79d186be2e5cba7fe2b2dbb2f56160f
Ivan Naranjo <ivann@google.com>
2016-10-25 10:37:52 +0100

    Organizing usings.

--- GoogleCloudExtension/GoogleCloudExtension/Theming/CommonDialogWindowBaseContent.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Theming/CommonDialogWindowBaseContent.cs
@@ -1,11 +1,5 @@
 ﻿using GoogleCloudExtension.Utils;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
