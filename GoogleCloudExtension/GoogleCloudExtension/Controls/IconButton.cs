78f5d0998af3c457c8e43721bd23dd706d406a7d
Ivan Naranjo <ivann@google.com>
2016-11-04 11:54:51 +0000

    Adding the button that can alternate icons.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Controls/IconButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GoogleCloudExtension.Controls
+{
+    public class IconButton : Button
+    {
+        public static readonly DependencyProperty NormalIconProperty =
+            DependencyProperty.Register(
+                nameof(NormalIcon),
+                typeof(ImageSource),
+                typeof(IconButton));
+
+        public static readonly DependencyProperty MouseOverIconProperty =
+            DependencyProperty.Register(
+                nameof(MouseOverIcon),
+                typeof(ImageSource),
+                typeof(IconButton));
+
+        public ImageSource NormalIcon
+        {
+            get { return (ImageSource)GetValue(NormalIconProperty); }
+            set { SetValue(NormalIconProperty, value); }
+        }
+
+        public ImageSource MouseOverIcon
+        {
+            get { return (ImageSource)GetValue(MouseOverIconProperty); }
+            set { SetValue(MouseOverIconProperty, value); }
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/Extensions/ImageResource.cs
@@ -0,0 +1,28 @@
+﻿using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace GoogleCloudExtension.Extensions
+{
+    public class ImageResource : MarkupExtension
+    {
+        public string Path { get; set; }
+
+        public ImageResource()
+        { }
+
+        public ImageResource(string path)
+        {
+            Path = path;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ResourceUtils.LoadImage(Path);
+        }
+    }
+}
