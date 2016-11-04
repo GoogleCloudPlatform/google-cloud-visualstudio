22043da27e39557fcd311b3a2a8d83f722308682
Ivan Naranjo <ivann@google.com>
2016-11-04 12:06:57 +0000

    Adding comments.

--- GoogleCloudExtension/GoogleCloudExtension/Controls/IconButton.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Controls/IconButton.cs
@@ -9,6 +9,10 @@ using System.Windows.Media;
 
 namespace GoogleCloudExtension.Controls
 {
+    /// <summary>
+    /// This class represents a specialization of the button control that only shows icons, showing
+    /// different icons depending on the visual state.
+    /// </summary>
     public class IconButton : Button
     {
         public static readonly DependencyProperty NormalIconProperty =
@@ -23,12 +27,18 @@ namespace GoogleCloudExtension.Controls
                 typeof(ImageSource),
                 typeof(IconButton));
 
+        /// <summary>
+        /// The icon to show in the normal state.
+        /// </summary>
         public ImageSource NormalIcon
         {
             get { return (ImageSource)GetValue(NormalIconProperty); }
             set { SetValue(NormalIconProperty, value); }
         }
 
+        /// <summary>
+        /// The icon to show in the mouse over state.
+        /// </summary>
         public ImageSource MouseOverIcon
         {
             get { return (ImageSource)GetValue(MouseOverIconProperty); }
--- GoogleCloudExtension/GoogleCloudExtension/Extensions/ImageResource.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Extensions/ImageResource.cs
@@ -5,11 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
+using System.Windows.Media;
 
 namespace GoogleCloudExtension.Extensions
 {
+    /// <summary>
+    /// This class is a simple markup extension that returns an image given its path.
+    /// </summary>
     public class ImageResource : MarkupExtension
     {
+        // Global cache of images.
+        private static readonly Dictionary<string, ImageSource> s_cache = new Dictionary<string, ImageSource>();
+
+        /// <summary>
+        /// The path to load.
+        /// </summary>
         public string Path { get; set; }
 
         public ImageResource()
@@ -20,9 +30,22 @@ namespace GoogleCloudExtension.Extensions
             Path = path;
         }
 
+        /// <summary>
+        /// Loads the image given the <seealso cref="Path"/> property. Caching is performed so an image is
+        /// only going to be loaded once.
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            return ResourceUtils.LoadImage(Path);
+            ImageSource result = null;
+            if (!s_cache.TryGetValue(Path, out result))
+            {
+                result = ResourceUtils.LoadImage(Path);
+                s_cache[Path] = result;
+            }
+
+            return result;
         }
     }
 }
