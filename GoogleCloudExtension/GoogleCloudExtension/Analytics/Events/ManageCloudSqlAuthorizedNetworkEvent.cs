abdf7e2663e3c89dd09afa869bb4e76b30716ba2
Ivan Naranjo <ivann@google.com>
2016-11-16 14:55:35 +0000

    Adding events for Cloud SQL.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Analytics/Events/ManageCloudSqlAuthorizedNetworkEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.Analytics.Events
+{
+    /// <summary>
+    /// Event sent when the authorized networks for a Cloud SQL instance are changed.
+    /// </summary>
+    internal static class ManageCloudSqlAuthorizedNetworkEvent
+    {
+        private const string ManageCloudSqlAuthorizedNetworkEventName = "manageCloudSqlAuthorizedNetwork";
+
+        public static AnalyticsEvent Create(CommandStatus status)
+        {
+            return new AnalyticsEvent(
+                ManageCloudSqlAuthorizedNetworkEventName,
+                CommandStatusUtils.StatusProperty, CommandStatusUtils.GetStatusString(status));
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/Analytics/Events/OpenCloudSqlConnectionDialogEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.Analytics.Events
+{
+    /// <summary>
+    /// Event sent when the cloud SQL connection dialog is open.
+    /// </summary>
+    internal static class OpenCloudSqlConnectionDialogEvent
+    {
+        private const string OpenCloudSqlConnectionDialogEventName = "openCloudSqlConnectionDialog";
+
+        public static AnalyticsEvent Create()
+        {
+            return new AnalyticsEvent(OpenCloudSqlConnectionDialogEventName);
+        }
+    }
+}
