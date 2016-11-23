7ed95c756d5b86782acfee5ff217301c454db5bf
Ivan Naranjo <ivann@google.com>
2016-11-23 14:32:19 +0000

    Updating tempalte, using the control in the Windows credentials dialog.

--- GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoogleCloudExtension
-{
-
-
+namespace GoogleCloudExtension {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -1960,7 +1960,7 @@ namespace GoogleCloudExtension
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to _Credentials for {0}. .
+        ///   Looks up a localized string similar to _Credentials for {0}: .
         /// </summary>
         public static string ManageWindowsCredentialsWindowMessage {
             get {
