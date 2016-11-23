d7d99da75429c99cf1f4621758a090bf80c9a405
Ivan Naranjo <ivann@google.com>
2016-11-23 13:55:37 +0000

    Initial drop of the control and image.

---
+++ GoogleCloudExtension/GoogleCloudExtension/Controls/LabelWithHelp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GoogleCloudExtension.Controls
+{
+    /// <summary>
+    /// This control represents a label with a help anchor to show help messages to the user.
+    /// </summary>
+    public class LabelWithHelp : System.Windows.Controls.Label
+    {
+        public static readonly DependencyProperty HelpContentProperty =
+            DependencyProperty.Register(
+                nameof(HelpContent),
+                typeof(object),
+                typeof(LabelWithHelp));
+
+        /// <summary>
+        /// This property contains the help content to be shown when hovering over the help anchor.
+        /// </summary>
+        public object HelpContent
+        {
+            get { return GetValue(HelpContentProperty); }
+            set { SetValue(HelpContentProperty, value); }
+        }
+    }
+}
