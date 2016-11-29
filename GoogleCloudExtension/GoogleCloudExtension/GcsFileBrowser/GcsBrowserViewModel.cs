b2e81f1a6fd5cac137e7018680729ed8ffb1051d
Ivan Naranjo <ivann@google.com>
2016-11-29 13:28:37 +0000

    Initial visuals for the browser.

--- GoogleCloudExtension/GoogleCloudExtension/GcsFileBrowser/GcsBrowserViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/GcsFileBrowser/GcsBrowserViewModel.cs
@@ -2,6 +2,7 @@
 using GoogleCloudExtension.Utils;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,29 @@ namespace GoogleCloudExtension.GcsFileBrowser
     public class GcsBrowserViewModel : ViewModelBase
     {
         private Bucket _bucket;
+        private IEnumerable<string> _pathSteps;
 
         public Bucket Bucket
         {
             get { return _bucket; }
-            set { SetValueAndRaise(ref _bucket, value); }
+            set
+            {
+                SetValueAndRaise(ref _bucket, value);
+                InvalidateBucket();
+            }
+        }
+
+        public IEnumerable<string> PathSteps
+        {
+            get { return _pathSteps; }
+            set { SetValueAndRaise(ref _pathSteps, value); }
+        }
+
+        public ObservableCollection<GcsItem> Items { get; } = new ObservableCollection<GcsItem>();
+
+        private void InvalidateBucket()
+        {
+                        
         }
     }
 }
---
+++ GoogleCloudExtension/GoogleCloudExtension/GcsFileBrowser/GcsItem.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Storage.v1.Data;
+using GoogleCloudExtension.Utils;
+
+namespace GoogleCloudExtension.GcsFileBrowser
+{
+    public class GcsItem
+    {
+        public string Name { get; }
+
+        public bool IsDirectory { get; }
+
+        public ulong Size { get; }
+
+        public string LastModified { get; }
+
+        public GcsItem(string name)
+        {
+            Name = name;
+            IsDirectory = true;
+        }
+
+        public GcsItem(Object obj)
+        {
+            Name = obj.Name;
+            IsDirectory = false;
+            Size = obj.Size ?? 0;
+            LastModified = obj.Updated?.ToString() ?? "Unknown";
+        }
+    }
+}
