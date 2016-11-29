1c3906c1537d378a69d2b51eef341fd11aa9fd7f
Ivan Naranjo <ivann@google.com>
2016-11-29 16:54:51 +0000

    Navigate directories, down.

--- GoogleCloudExtension/GoogleCloudExtension/GcsFileBrowser/GcsItem.cs
+++ GoogleCloudExtension/GoogleCloudExtension/GcsFileBrowser/GcsItem.cs
@@ -1,11 +1,14 @@
 ﻿using Google.Apis.Storage.v1.Data;
 using GoogleCloudExtension.Utils;
+using System.Linq;
 
 namespace GoogleCloudExtension.GcsFileBrowser
 {
     public class GcsItem
     {
         public string Name { get; }
+
+        public string FileName { get; }
 
         public bool IsDirectory { get; }
 
@@ -17,6 +20,7 @@ namespace GoogleCloudExtension.GcsFileBrowser
         {
             Name = name;
             IsDirectory = true;
+            FileName = GetLeafName(Name);
         }
 
         public GcsItem(Object obj)
@@ -25,6 +29,13 @@ namespace GoogleCloudExtension.GcsFileBrowser
             IsDirectory = false;
             Size = obj.Size ?? 0;
             LastModified = obj.Updated?.ToString() ?? "Unknown";
+            FileName = GetLeafName(Name);
+        }
+
+        private static string GetLeafName(string name)
+        {
+            var cleanName = name.Last() == '/' ? name.Substring(0, name.Length - 1) : name;
+            return cleanName.Split('/').Last();
         }
     }
 }
