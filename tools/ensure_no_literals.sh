93d8fe1a0c15b47d88153326795c9fa02b54434e
Ivan Naranjo <ivann@google.com>
2016-12-08 14:18:08 +0000

    Simplify the find expression.

--- tools/ensure_no_literals.sh
+++ tools/ensure_no_literals.sh
@@ -4,13 +4,11 @@
 readonly workspace=$(dirname $0)/..
 
 failed=0
-for i in $(find ${workspace}/GoogleCloudExtension/ -maxdepth 1 -mindepth 1 -type d -name 'Google*'); do
-    if [ -n "$(${workspace}/tools/find_strings.sh $i)" ]; then
-	echo "Found missing strings in $i"
-	${workspace}/tools/find_strings.sh $i
-	failed=1
-    fi
-done
+if [ -n "$(${workspace}/tools/find_strings.sh ${workspace}/GoogleCloudExtension/GoogleCloudExtension)" ]; then
+    echo "Found missing strings in $i"
+    ${workspace}/tools/find_strings.sh $i
+    failed=1
+fi
 
 if [ "$failed" -eq "1" ]; then
     echo "Found strings that are not translated."
