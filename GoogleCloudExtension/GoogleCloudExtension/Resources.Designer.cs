5699e452ce1891f2811d8a7e6ac0f466e0524fa0
Deren Liao <derenl@google.com>
2016-12-14 01:00:03 -0800

    Add LogItem message format spaces

--- GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
@@ -1933,7 +1933,7 @@ namespace GoogleCloudExtension {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0}: {1}.
+        ///   Looks up a localized string similar to {0}: {1}  .
         /// </summary>
         public static string LogViewerDictionaryPayloadFormatString {
             get {
