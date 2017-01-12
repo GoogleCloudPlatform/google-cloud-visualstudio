cd135d295ce0bfd9a0100eea524b7403fe6ef754
Ivan Naranjo <ivann@google.com>
2017-01-12 14:23:01 +0000

    Adding icon for GKE button.

--- GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/ChoiceStep/ChoiceStepViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/PublishDialogSteps/ChoiceStep/ChoiceStepViewModel.cs
@@ -34,9 +34,11 @@ namespace GoogleCloudExtension.PublishDialogSteps.ChoiceStep
     {
         private const string AppEngineIconPath = "PublishDialogSteps/ChoiceStep/Resources/AppEngine_128px_Retina.png";
         private const string GceIconPath = "PublishDialogSteps/ChoiceStep/Resources/ComputeEngine_128px_Retina.png";
+        private const string GkeIconPath = "PublishDialogSteps/ChoiceStep/Resources/ContainerEngine_128px_Retina.png";
 
         private static readonly Lazy<ImageSource> s_appEngineIcon = new Lazy<ImageSource>(() => ResourceUtils.LoadImage(AppEngineIconPath));
         private static readonly Lazy<ImageSource> s_gceIcon = new Lazy<ImageSource>(() => ResourceUtils.LoadImage(GceIconPath));
+        private static readonly Lazy<ImageSource> s_gkeIcon = new Lazy<ImageSource>(() => ResourceUtils.LoadImage(GkeIconPath));
 
         private readonly ChoiceStepContent _content;
         private IPublishDialog _dialog;
@@ -77,6 +79,8 @@ namespace GoogleCloudExtension.PublishDialogSteps.ChoiceStep
                     Command = new ProtectedCommand(
                         OnGkeChoiceCommand,
                         canExecuteCommand: projectType == KnownProjectTypes.NetCoreWebApplication),
+                    Icon = s_gkeIcon.Value,
+                    ToolTip = "Deploy as a service to Container Engine"
                 },
                 new Choice
                 {
