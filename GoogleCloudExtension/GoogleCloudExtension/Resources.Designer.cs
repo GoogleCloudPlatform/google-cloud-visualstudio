9a55a3966237ebf8c62a22e2f6ae5caa68ae045b
Ivan Naranjo <ivannaranjo@users.noreply.github.com>
2017-02-14 11:02:24 +0000

    Take into account the "name" property from project.json. (#418)
    * Adding functionality to read the name property of the project.json.
    
    * Sorting and organizing usings.
    
    * Adding missing licenses.
    
    * Better comments.

--- GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoogleCloudExtension {
-    using System;
-    
-    
+namespace GoogleCloudExtension
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
