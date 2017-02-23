9469398cb31130e4b1ce41e21bcde8d66e421b9c
Ivan Naranjo <ivann@google.com>
2017-02-23 15:07:09 +0000

    Adding initial check for valid config.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspnetMvc/Global.asax.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspnetMvc/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Xml;
+using System.Configuration;
 
 namespace $safeprojectname$
 {
@@ -12,6 +14,18 @@ namespace $safeprojectname$
     {
         protected void Application_Start()
         {
+	    // Configure Stackdriver Logging via Log4Net
+            var section = (XmlElement)ConfigurationManager.GetSection("log4net");
+            XmlElement element =
+                (XmlElement)section.GetElementsByTagName("projectId").Item(0);
+            string projectId = element.Attributes["value"].Value;
+            // Configure logging only if projectId has been changed from placeholder value. 
+            if (projectId != ("YOUR-PROJECT-ID"))
+            {
+                // Configure log4net to use Stackdriver logging from the XML configuration file.
+                log4net.Config.XmlConfigurator.Configure();
+            }
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
