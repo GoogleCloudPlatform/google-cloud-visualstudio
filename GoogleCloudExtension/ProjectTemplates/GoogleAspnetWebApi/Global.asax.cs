99ef4c9cea05e0fdd456680e4a994d07b92be973
Ivan Naranjo <ivann@google.com>
2017-02-23 13:50:42 +0000

    Adding missing usings.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Global.asax.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspnetWebApi/Global.asax.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Xml;
+using System.Xml;
+using System.Configuration;
 
 namespace $safeprojectname$
 {
