21882aa7675c5eab10def02fc9a56e492a523e0b
Deren-Liao <derenl@google.com>
2017-02-23 19:03:43 -0800

    Stackframe parser

---
+++ GoogleCloudExtension/GoogleCloudExtension/StackdriverErrorReporting/GotoSourceLocation/StackframeParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.StackdriverErrorReporting.GotoSourceLocation
+{
+    class StackframeParser
+    {
+    }
+}
