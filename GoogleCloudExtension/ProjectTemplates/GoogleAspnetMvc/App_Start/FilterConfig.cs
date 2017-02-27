4eeba713df7c78897535f1faa0a5c3f605c18174
Ivan Naranjo <ivann@google.com>
2017-02-27 13:46:49 +0000

    Updating MVC to use the projectId from the ambient.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspnetMvc/App_Start/FilterConfig.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspnetMvc/App_Start/FilterConfig.cs
@@ -11,20 +11,30 @@ namespace $safeprojectname$
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-	    // To enable Google Cloud Stackdriver Error Reporting edit
-	    // Web.config and replace "YOUR-PROJECT-ID" with your Google Cloud
-	    // Project ID
+	    // To enable Google Cloud Stackdrive Logging and Error Reporting
+	    // while running on your local machine edit Web.config and uncomment
+	    // the <projectId> value under the <log4net> section. Ensure that
+	    // the <projectId> is set to a valid Google Cloud Project Id.
 
             // [START error_reporting]
             // Check to ensure that projectId has been changed from placeholder value.
             var section = (XmlElement)ConfigurationManager.GetSection("log4net");
             XmlElement element =
                 (XmlElement)section.GetElementsByTagName("projectId").Item(0);
-            string projectId = element.Attributes["value"].Value;
+            string projectId = element?.Attributes["value"].Value;
             if (projectId == ("YOUR-PROJECT-ID"))
             {
                 throw new Exception("Update Web.config and replace YOUR-PROJECT-ID"
                    + " with your Google Cloud Project ID, and recompile.");
+            }
+            if (projectId == null)
+            {
+                projectId = Google.Api.Gax.Platform.Instance().GceDetails?.ProjectId;
+                if (projectId == null)
+                {
+                    throw new Exception("The logging and error reporting libraries need a project ID. "
+                        + "Update Web.config and add a <projectId> entry in the <log4net> section.");
+                }
             }
 	    var serviceName = ConfigurationManager.AppSettings["google_error_reporting:serviceName"];
 	    var version = ConfigurationManager.AppSettings["google_error_reporting:version"];
