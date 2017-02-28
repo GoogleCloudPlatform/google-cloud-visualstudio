1a8fd298c1a49824bac070847665345221fbb174
Ivan Naranjo <ivann@google.com>
2017-02-28 13:53:38 +0000

    Adding logging to Web API template.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreWebApi/Startup.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreWebApi/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Cloud.Diagnostics.AspNetCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,32 +13,66 @@ namespace $safeprojectname$
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+	public Startup(IHostingEnvironment env)
+	{
+	    var builder = new ConfigurationBuilder()
+		.SetBasePath(env.ContentRootPath)
+		.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+		.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+		.AddEnvironmentVariables();
+	    Configuration = builder.Build();
+	}
 
-        public IConfigurationRoot Configuration { get; }
+	public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
+	// This method gets called by the runtime. Use this method to add services to the container.
+	public void ConfigureServices(IServiceCollection services)
+	{
+	    // Add framework services.
+	    services.AddMvc();
+	    services.AddGoogleTrace(GetProjectId());
+	}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+	public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+	{
+	    var projectId = GetProjectId();
 
-            app.UseMvc();
-        }
+	    loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+	    loggerFactory.AddGoogle(projectId);
+	    loggerFactory.AddDebug();
+
+	    if (env.IsDevelopment())
+	    {
+		app.UseGoogleExceptionLogging(projectId,
+		    Configuration["GoogleErrorReporting:ServiceName"],
+		    Configuration["GoogleErrorReporting:Version"]);
+	    }
+
+	    app.UseGoogleTrace();
+
+	    app.UseMvc();
+	}
+
+	private string GetProjectId()
+	{
+	    string projectId = Configuration["ProjectId"];
+	    if (projectId == ("YOUR-PROJECT-ID"))
+	    {
+		throw new Exception("Update appsettings.json and replace YOUR-PROJECT-ID"
+		    + " with your Google Cloud Project ID, and recompile.");
+	    }
+	    if (projectId == null)
+	    {
+		projectId = Google.Api.Gax.Platform.Instance().GceDetails?.ProjectId;
+		if (projectId == null)
+		{
+		    throw new Exception("The logging, tracing and error reporting libraries need a project ID. "
+			+ "Update appsettings.json and replace YOUR-PROJECT-ID with your "
+			+ "Google Cloud Project ID, and recompile.");
+		}
+	    }
+	    return projectId;
+	}
     }
 }
