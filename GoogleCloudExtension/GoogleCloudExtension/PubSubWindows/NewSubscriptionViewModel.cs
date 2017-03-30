c4e65477753361fb033015fa7ba681cec3bbcf6e
Jim Przybylinski <przybjw@google.com>
2017-03-30 08:46:42 -0700

    Pubsub feature (#520)
    * Pub sub (#186)
    
    Add Pub/Sub tree element.
    Add topics under Pub/Sub tree element.
    Add listing subscriptions by topic.
    
    * Pub sub add/delete topics and subscriptions. (#202)
    
    * Add create and delete windows for PubSub topics and subscriptions.
    
    * Update code to work with new changes in master.
    
    * Resolve comments from Deren.
    
    * Resolve Ivann PR comments.
    
    * Make all validation messages end with a period.
    
    * Add changes based on UI review.
    
    * Change acknowledgement deadline label to be a LabelWithHelp.

---
+++ GoogleCloudExtension/GoogleCloudExtension/PubSubWindows/NewSubscriptionViewModel.cs
@@ -0,0 +1,119 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Pubsub.v1.Data;
+using GoogleCloudExtension.CloudExplorerSources.PubSub;
+using GoogleCloudExtension.Theming;
+using GoogleCloudExtension.Utils;
+using System.Linq;
+
+namespace GoogleCloudExtension.PubSubWindows
+{
+    /// <summary>
+    /// Data objet that backs the new subscription window. It contains the information needed to create a new
+    /// subscription.
+    /// </summary>
+    public class NewSubscriptionViewModel : ViewModelBase
+    {
+        private readonly CommonDialogWindowBase _owner;
+
+        /// <summary>
+        /// The name of the topic the subscription belongs to.
+        /// </summary>
+        public string TopicName => PubsubDataSource.GetPathLeaf(Subscription.Topic);
+
+        /// <summary>
+        /// If PubSub should send a push notification rather than waiting for a pull.
+        /// </summary>
+        public bool Push
+        {
+            get { return Subscription.PushConfig == PushConfig; }
+            set
+            {
+                if (value != Push)
+                {
+                    Subscription.PushConfig = value ? PushConfig : null;
+                    RaisePropertyChanged(nameof(Push));
+                }
+            }
+        }
+
+        /// <summary>
+        /// The PushConfig object that will be part of the subscription if the
+        /// push option is selected. The object always exists so we don't lose
+        /// data if the user temporarily selects the pull option.
+        /// </summary>
+        public PushConfig PushConfig { get; }
+
+        /// <summary>
+        /// The Subscription object this view model models.
+        /// </summary>
+        public Subscription Subscription { get; }
+
+        /// <summary>
+        /// The command called to actually create the subscription.
+        /// </summary>
+        public ProtectedCommand CreateCommand { get; }
+
+        /// <summary>
+        /// The modeled subscription. Null until the create command is run.
+        /// </summary>
+        public Subscription Result { get; private set; }
+
+        /// <summary>
+        /// Creates a new NewSubscriptionViewModel
+        /// </summary>
+        /// <param name="subscription">The subscription object to model</param>
+        /// <param name="owner">The owner dialog.</param>
+        public NewSubscriptionViewModel(Subscription subscription, CommonDialogWindowBase owner)
+        {
+            _owner = owner;
+            Subscription = subscription;
+            CreateCommand = new ProtectedCommand(OnCreateCommand);
+            PushConfig = subscription.PushConfig ?? new PushConfig();
+        }
+
+        /// <summary>
+        /// The execution of the CreateCommand.
+        /// </summary>
+        private void OnCreateCommand()
+        {
+            if (ValidateInput())
+            {
+                Result = Subscription;
+                _owner.Close();
+            }
+        }
+
+        /// <summary>
+        /// Validates the model input data. Prompts the user on validation errors.
+        /// </summary>
+        /// <returns>True if the data is valid, false if the user was prompted about invalid data.</returns>
+        private bool ValidateInput()
+        {
+            var results = PubSubNameValidationRule.Validate(Subscription.Name);
+            var details = string.Join("\n", results.Select(result => result.Message));
+            if (!string.IsNullOrEmpty(details))
+            {
+                string message = string.Format(Resources.PubSubNewSubscriptionNameInvalidMessage, Subscription.Name);
+                UserPromptUtils.ErrorPrompt(message, Resources.PubSubNewSubscriptionNameInvalidTitle, details);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/PubSubWindows/PubSubNameValidationRule.cs
@@ -0,0 +1,88 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace GoogleCloudExtension.PubSubWindows
+{
+    /// <summary>
+    /// A validation rule that checks that a name fits the common google naming rules.
+    /// </summary>
+    public class PubSubNameValidationRule : ValidationRule
+    {
+        /// <summary>
+        /// Override for validation rule. Exists if WPF binding validation becomes usable.
+        /// </summary>
+        /// <param name="value">The name to validate.</param>
+        /// <param name="cultureInfo">Unused.</param>
+        /// <returns></returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return Validate(value).FirstOrDefault() ?? ValidationResult.ValidResult;
+        }
+
+        /// <summary>
+        /// From the pub sub api documentation,
+        /// both subscription names and topic names must follow these rules:
+        /// It must start with a letter.
+        /// It must contain only letters (`[A-Za-z]`), numbers (`[0-9]`), dashes (`-`), underscores (`_`),
+        /// periods (`.`), tildes (`~`), plus (`+`) or percent signs (`%`).
+        /// It must be between 3 and 255 characters in length.
+        /// It must not start with `"goog"`.
+        /// </summary>
+        public static IEnumerable<StringValidationResult> Validate(object value)
+        {
+            string name = value?.ToString();
+            if (name == null)
+            {
+                yield return new StringValidationResult(Resources.ValidationThreeCharactersMessage);
+                yield break;
+            }
+            if (name.Length < 3)
+            {
+                yield return new StringValidationResult(Resources.ValidationThreeCharactersMessage);
+            }
+            if (name.Length > 255)
+            {
+                yield return new StringValidationResult(Resources.Validation255CharactersMessage);
+            }
+            if (!char.IsLetter(name.First()))
+            {
+                yield return new StringValidationResult(Resources.ValidationStartWithLetterMessage);
+            }
+            if (Regex.IsMatch(name, "[^A-Za-z0-9_\\.~+%\\-]"))
+            {
+                yield return new StringValidationResult(Resources.ValidationPubSubNameCharacterClassMessage);
+            }
+            if (name.StartsWith("goog"))
+            {
+                yield return new StringValidationResult(Resources.ValidationDisallowStartGoogMessage);
+            }
+        }
+
+        public class StringValidationResult : ValidationResult
+        {
+            public string Message { get; }
+
+            public StringValidationResult(string errorContent) : base(false, errorContent)
+            {
+                Message = errorContent;
+            }
+        }
+    }
+}
