da5b6d633d73db40dd300cdb16cfeedb3c6100f3
Ivan Naranjo <ivann@google.com>
2017-04-07 13:28:35 +0100

    Making the MVC VS 2017 template work.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Program.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Program.cs
@@ -16,7 +16,6 @@ namespace $safeprojectname$
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
--- GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Startup.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Cloud.Diagnostics.AspNetCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,12 +30,16 @@ namespace $safeprojectname$
         {
             // Add framework services.
             services.AddMvc();
+            services.AddGoogleTrace(GetProjectId());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            var projectId = GetProjectId();
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddGoogle(projectId);
             loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
@@ -45,9 +50,13 @@ namespace $safeprojectname$
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseGoogleExceptionLogging(projectId,
+                    Configuration["GoogleErrorReporting:ServiceName"],
+                    Configuration["GoogleErrorReporting:Version"]);
             }
 
             app.UseStaticFiles();
+            app.UseGoogleTrace();
 
             app.UseMvc(routes =>
             {
@@ -55,6 +64,28 @@ namespace $safeprojectname$
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private string GetProjectId()
+        {
+            string projectId = Configuration["ProjectId"];
+            if (projectId == ("YOUR-PROJECT-ID"))
+            {
+                throw new Exception("Update appsettings.json and replace YOUR-PROJECT-ID"
+                    + " with your Google Cloud Project ID, and recompile.");
+            }
+            if (projectId == null)
+            {
+                var instance = Google.Api.Gax.Platform.Instance();
+                projectId = instance.GceDetails?.ProjectId ?? instance.GaeDetails?.ProjectId;
+                if (projectId == null)
+                {
+                    throw new Exception("The logging, tracing and error reporting libraries need a project ID. "
+                        + "Update appsettings.json and replace YOUR-PROJECT-ID with your "
+                        + "Google Cloud Project ID, and recompile.");
+                }
+            }
+            return projectId;
         }
     }
 }
