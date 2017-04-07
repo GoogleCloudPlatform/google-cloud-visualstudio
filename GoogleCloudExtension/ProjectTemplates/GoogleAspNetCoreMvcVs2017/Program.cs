bde2fe4c4b70e10901ab54da7816a01d6bfc5b8a
Ivan Naranjo <ivann@google.com>
2017-04-07 14:38:36 +0100

    Adding back the insights.

--- GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Program.cs
+++ GoogleCloudExtension/ProjectTemplates/GoogleAspNetCoreMvcVs2017/Program.cs
@@ -16,6 +16,7 @@ namespace $safeprojectname$
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
