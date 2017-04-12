4c83a0cde2dda65c7eadc5280b4e58ba258d1de3
Deren-Liao <derenl@google.com>
2017-04-12 03:26:11 +0100

    Code refactoring with code review feedback

---
+++ GoogleCloudExtension/GoogleCloudExtension.GitUtils/GitCommit.cs
@@ -0,0 +1,106 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.GitUtils
+{
+    /// <summary>
+    /// Represent a local git commit on a given git SHA.
+    /// </summary>
+    internal class GitCommit
+    {
+        private readonly GitRepository _repo;
+        private readonly string _sha;
+        private HashSet<string> _fileTree;
+        
+        internal GitCommit(GitRepository gitCommand, string sha)
+        {
+            _repo = gitCommand.ThrowIfNull(nameof(gitCommand));
+            _sha = sha.ThrowIfNullOrEmpty(nameof(sha));
+        }
+
+        /// <summary>
+        /// Search for a local git repository that contains a Git Commit.
+        /// </summary>
+        /// <param name="path">The path to search for local git repo.</param>
+        /// <param name="sha">The git commit SHA to be searched for.</param>
+        /// <returns>
+        /// A <seealso cref="GitCommit"/> object that represents the git SHA commit at a local repository.
+        /// null if the search is not successful.
+        /// </returns>
+        public static async Task<GitCommit> FindCommit(string path, string sha)
+        {
+            string dir = path.ThrowIfNullOrEmpty(nameof(path));
+            sha.ThrowIfNullOrEmpty(nameof(sha));
+            var gitCommand = await GitRepository.GetGitCommandWrapperForPathAsync(dir);
+            return await gitCommand?.ContainsCommitAsync(sha) == true ? new GitCommit(gitCommand, sha) : null;
+        }
+
+        /// <summary>
+        /// Search for all the possible suffix matching relative file paths.
+        /// </summary>
+        /// <param name="filePath">
+        /// The full file path to be searched for.
+        /// The path is from the machine that build the assembly.
+        /// So the file path has different root path.
+        /// </param>
+        /// <returns>File path relative to git root. </returns>
+        public async Task<IEnumerable<string>> FindMatchingEntryAsync(string filePath)
+        {
+            var fileTree = await GetFileTreeAsync();
+            return SubPaths(filePath.ToLowerInvariant()).Where(x => fileTree.Contains(x));
+        }
+
+        /// <summary>
+        /// Save the relative file content to a temporary file.
+        /// </summary>
+        /// <param name="tmpFile">Temporary file path.</param>
+        /// <param name="relativePath">The file path relative to the git root.</param>
+        public async Task SaveTempFileAsync(string tmpFile, string relativePath)
+        {
+            var content = await _repo.GetRevisionFileAsync(_sha, relativePath);
+            using (var sw = new StreamWriter(tmpFile))
+            {
+                content.ForEach(x => sw.WriteLine(x));
+            }
+        }
+
+        private async Task<HashSet<string>> GetFileTreeAsync()
+        {
+            if (_fileTree == null)
+            {
+                var query = (await _repo.ListTreeAsync(_sha))?.Select(x => x.Replace('/', '\\').ToLowerInvariant());
+                _fileTree = new HashSet<string>(query);
+            }
+            return _fileTree;
+        }
+
+        private static IEnumerable<string> SubPaths(string filePath)
+        {
+            filePath.ThrowIfNullOrEmpty(nameof(filePath));
+
+            int index = filePath.IndexOf(Path.DirectorySeparatorChar);
+            for (; index >= 0; index = filePath.IndexOf(Path.DirectorySeparatorChar, index+1))
+            {
+                yield return filePath.Substring(index + 1);
+            }
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension.GitUtils/GitRepository.cs
@@ -0,0 +1,119 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.GitUtils
+{
+    /// <summary>
+    /// A wrapper for executing git commands on a local git repository root.
+    /// </summary>
+    internal class GitRepository
+    {
+        private const string GitExecutable = "git.exe";
+        private const string GitDefaultLocation = @"Git\cmd\git.exe";
+
+        private static Lazy<string> s_gitPathLazy = new Lazy<string>(GetGitPath);
+
+        /// <summary>
+        /// Git repository local root path.
+        /// </summary>
+        public string Root { get; }
+
+        /// <summary>
+        /// Gets the git.exe full path if it is installed properly.
+        /// Return null if git.exe is not found.
+        /// </summary>
+        public static string GitPath => s_gitPathLazy.Value;
+
+        private GitRepository(string gitLocalRoot)
+        {
+            Root = gitLocalRoot;
+        }
+
+        /// <summary>
+        /// Returns a <seealso cref="GitRepository"/> object 
+        /// if the <paramref name="dir"/> is a valid locat Git repository path.
+        /// </summary>
+        /// <param name="dir">The file path to be checked.</param>
+        /// <returns>
+        /// <seealso cref="GitRepository"/> object or null if the path is not a valid git repository path.
+        /// </returns>
+        public static async Task<GitRepository> GetGitCommandWrapperForPathAsync(string dir)
+        {
+            if (await IsGitRepositoryAsync(dir))
+            {
+                var root =  (await RunGitCommandAsync("rev-parse --show-toplevel", dir)).FirstOrDefault()?.Replace('/', '\\');
+                return new GitRepository(root);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns true if the directory is under a local git repository folder.
+        /// </summary>
+        /// <param name="dir">The file directory.</param>
+        public static async Task<bool> IsGitRepositoryAsync(string dir) => (await RunGitCommandAsync("log -1", dir))?.Count > 0;
+
+        /// <summary>
+        /// Returns true if the git repository contains the git SHA revision.
+        /// </summary>
+        /// <param name="sha">The Git SHA.</param>
+        public async Task<bool> ContainsCommitAsync(string sha) => (await ExecCommandAsync($"cat-file -t {sha}")).FirstOrDefault() == "commit";
+
+        /// <summary>
+        /// Returns the items tree of a given git SHA revision.
+        /// </summary>
+        /// <param name="sha">The Git SHA.</param>
+        public Task<List<string>> ListTreeAsync(string sha) => ExecCommandAsync($"ls-tree -r {sha} --name-only");
+
+        /// <summary>
+        /// Returns the file content of a givin git SHA revision.
+        /// </summary>
+        /// <param name="sha">The Git SHA.</param>
+        /// <param name="relativePath">Relative path to git repository root.</param>
+        public Task<List<string>> GetRevisionFileAsync(string sha, string relativePath)
+            => ExecCommandAsync($"show {sha}:{relativePath.Replace('\\', '/')}");
+
+        private Task<List<string>> ExecCommandAsync(string command) => RunGitCommandAsync(command, Root);
+
+        private static string GetGitPath()
+        {
+            return Environment.GetEnvironmentVariable("PATH")
+                .Split(';')
+                .Select(x => Path.Combine(x, GitExecutable))
+                .FirstOrDefault(x => File.Exists(x));
+        }
+
+        /// <summary>
+        /// Run a git command and return the output or error output.
+        /// </summary>
+        private static async Task<List<string>> RunGitCommandAsync(
+            string command, 
+            string gitLocalRoot)
+        {
+            List<string> output = new List<string>();
+            return await ProcessUtils.RunCommandAsync(
+                    file: GitPath,
+                    args: command,
+                    handler: (o, e) => output.Add(e.Line),
+                    workingDir: gitLocalRoot) ? output: null;
+        }
+    }
+}
