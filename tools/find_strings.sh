e8db8938bb1eda0fa68089b2dee3fae3958e7919
Ivan Naranjo <ivann@google.com>
2017-05-09 14:02:42 +0100

    Exclude the obj and project templates directories.

--- tools/find_strings.sh
+++ tools/find_strings.sh
@@ -5,20 +5,23 @@
 
 readonly workspace=$(dirname $0)/../
 
+readonly exclude_paths=("obj" "ProjectTemplates")
+readonly find_files_params="-e .cs -x ${exclude_paths[@]}"
+
 # Look for literal strings on .cs files.
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "caption: \"|message: \"|title: \"|Header = \"|Caption = \""
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "caption: \\$\"|message: \\$\"|title: \"|Header = \\$\"|Caption = \\$\""
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "\\[Category\\(|\\[DisplayName\\(|\\[Description\\("
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "Prompt\\(\""
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "base\\(\""
-${workspace}/tools/find_files.py -d $1 -e .cs | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 ${find_files_params} | xargs grep -HnE \
     "\\.OutputLine\\(\\$\"|\\.OutputLine\\(\""
 
 # Look for literal strings on .xaml files.
-${workspace}/tools/find_files.py -d $1 -e .xaml | xargs grep -HnE \
+${workspace}/tools/find_files.py -d $1 -e .xaml -x ${exclude_paths[@]} | xargs grep -HnE \
     "Header=\"[A-Z]|Content=\"[A-Z]|Text=\"[A-Z]"
