c213274a88da3234f862e94a31b5b34ec99dd450
Deren Liao <derenl@google.com>
2017-06-01 21:26:43 +0100

    no message

--- GoogleCloudExtension/GoogleCloudExtension/Utils/StringUtils.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Utils/StringUtils.cs
@@ -15,8 +15,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security;
 using System.Text;
 
 namespace GoogleCloudExtension.Utils
