8672a5051b062c2b1155cb9ff57933bfbc7e3d9c
Deren Liao <derenl@google.com>
2017-06-08 21:08:45 +0100

    strings

--- GoogleCloudExtension/GoogleCloudExtension/Utils/ValidateGitDependencyHelper.cs
+++ GoogleCloudExtension/GoogleCloudExtension/Utils/ValidateGitDependencyHelper.cs
@@ -1,23 +1,43 @@
-﻿using GoogleCloudExtension.GitUtils;
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GoogleCloudExtension.GitUtils;
 using GoogleCloudExtension.LinkPrompt;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GoogleCloudExtension.Utils
 {
+    /// <summary>
+    /// Helper functions that validate if git for windows is installed.
+    /// </summary>
     public static class ValidateGitDependencyHelper
     {
+        /// <summary>
+        /// Asking to install Git for Windows if git.exe is not found
+        /// </summary>
+        /// <returns>
+        /// True: It is installed properly.
+        /// Otherwise, returns false.
+        /// </returns>
         public static bool ValidateGitForWindowsInstalled()
         {
             if (String.IsNullOrWhiteSpace(GitRepository.GetGitPath()))
             {
                 LinkPromptDialogWindow.PromptUser(
-                        Resources.GcloudMissingGcloudErrorTitle,
-                        Resources.GcloudMissingCloudSdkErrorMessage,
-                        new LinkInfo(link: "https://git-scm.com/download/win", caption: Resources.GcloudInstallLinkCaption));
+                        Resources.GitUtilsMissingGitErrorTitle,
+                        Resources.GitUtilsMissingGitErrorMessage,
+                        new LinkInfo(link: "https://git-scm.com/download/win", caption: Resources.GitUtilsGitInstallLinkCaption));
                 return false;
             }
             return true;
