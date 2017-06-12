d48dcef9a2075266e589fe5688e48a8cc7bcb7a0
Jim Przybylinski <JimWP@google.com>
2017-06-12 09:25:49 -0700

    Add missing licenses and class comments.

--- GoogleCloudExtension/GoogleCloudExtension.DataSources.UnitTests/DataSourceUnitTestsBase.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources.UnitTests/DataSourceUnitTestsBase.cs
@@ -1,3 +1,17 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using Google;
 using Google.Apis.Http;
 using Google.Apis.Requests;
@@ -13,6 +27,10 @@ using System.Threading.Tasks;
 
 namespace GoogleCloudExtension.DataSources.UnitTests
 {
+    /// <summary>
+    /// A base class for testing data sources.
+    /// This class includes methods for mocking Apiary services.
+    /// </summary>
     public class DataSourceUnitTestsBase
     {
         protected const string MockExceptionMessage = "MockException";
--- GoogleCloudExtension/GoogleCloudExtension.DataSources.UnitTests/PubSubDataSourceUnitTests.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources.UnitTests/PubSubDataSourceUnitTests.cs
@@ -1,4 +1,18 @@
-﻿using Google.Apis.Pubsub.v1;
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Pubsub.v1;
 using Google.Apis.Pubsub.v1.Data;
 using GoogleCloudExtension.CloudExplorerSources.PubSub;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,10 +22,12 @@ using System.Threading.Tasks;
 
 namespace GoogleCloudExtension.DataSources.UnitTests
 {
+    /// <summary>
+    /// Test class for <see cref="PubsubDataSource"/>
+    /// </summary>
     [TestClass]
     public class PubSubDataSourceUnitTests : DataSourceUnitTestsBase
     {
-        private static readonly Subscription s_newSubscription = new Subscription { Name = SubscriptionName, Topic = TopicName };
         private const string DummyString = "DummyString";
         private const string MockedTopicName = "MockedTopicName";
         private const string MockedSubscriptionName = "MockedSubscriptionName";
@@ -23,6 +39,7 @@ namespace GoogleCloudExtension.DataSources.UnitTests
         private const string TopicFullName = "projects/" + ProjectName + "/topics/" + TopicName;
         private const string SubscriptionName = "NewSubscriptionName";
         private const string SubscriptionFullName = "projects/" + ProjectName + "/subscriptions/" + SubscriptionName;
+        private static readonly Subscription s_newSubscription = new Subscription { Name = SubscriptionName, Topic = TopicName };
 
         [TestMethod]
         public async Task TestGetTopicListAsyncSinglePage()
