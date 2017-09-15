952737eedbab661f92732e97f9380344255439c2
Ivan Naranjo <ivann@google.com>
2017-09-15 14:22:41 +0100

    Remove unused variables, unnecessary complexity.

--- GoogleCloudExtension/GoogleCloudExtension.Deployment/GkeDeployment.cs
+++ GoogleCloudExtension/GoogleCloudExtension.Deployment/GkeDeployment.cs
@@ -114,11 +114,8 @@ namespace GoogleCloudExtension.Deployment
 
             using (var cleanup = new Disposable(() => CommonUtils.Cleanup(stageDirectory)))
             {
-                var appRootPath = Path.Combine(stageDirectory, "app");
-                var buildFilePath = Path.Combine(stageDirectory, "cloudbuild.yaml");
-
                 if (!await ProgressHelper.UpdateProgress(
-                        NetCoreAppUtils.CreateAppBundleAsync(project, appRootPath, toolsPathProvider, outputAction),
+                        NetCoreAppUtils.CreateAppBundleAsync(project, stageDirectory, toolsPathProvider, outputAction),
                         progress,
                         from: 0.1, to: 0.3))
                 {
@@ -126,7 +123,7 @@ namespace GoogleCloudExtension.Deployment
                     return null;
                 }
 
-                NetCoreAppUtils.CopyOrCreateDockerfile(project, appRootPath);
+                NetCoreAppUtils.CopyOrCreateDockerfile(project, stageDirectory);
                 var imageTag = CloudBuilderUtils.GetImageTag(
                     project: options.GCloudContext.ProjectId,
                     imageName: options.DeploymentName,
@@ -135,7 +132,7 @@ namespace GoogleCloudExtension.Deployment
                 if (!await ProgressHelper.UpdateProgress(
                     GCloudWrapper.BuildContainerAsync(
                         imageTag: imageTag,
-                        contentsPath: appRootPath,
+                        contentsPath: stageDirectory,
                         outputAction: outputAction,
                         context: options.GCloudContext),
                     progress,
