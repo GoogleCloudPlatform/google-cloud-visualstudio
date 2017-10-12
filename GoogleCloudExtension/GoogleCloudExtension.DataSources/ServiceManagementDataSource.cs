64402c5412246577357de6944661cb65b97ea3bf
Ivan Naranjo <ivann@google.com>
2017-10-12 16:30:18 +0100

    Initial drop of data source.

---
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/ServiceManagementDataSource.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.ServiceManagement.v1;
+using Google.Apis.ServiceManagement.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.DataSources
+{
+    public class ServiceManagementDataSource : DataSourceBase<ServiceManagementService>
+    {
+        public ServiceManagementDataSource(string projectId, GoogleCredential credential, string appName)
+            : base(projectId, credential, init => new ServiceManagementService(init), appName)
+        { }
+
+        public async Task<IList<ManagedService>> GetServicesAvailableAsync()
+        {
+            var request = Service.Services.List();
+
+            return await LoadPagedListAsync(
+                (token) =>
+                {
+                    request.PageToken = token;
+                    return request.ExecuteAsync();
+                },
+                x => x.Services,
+                x => x.NextPageToken);
+        }
+
+        public async Task<IList<ManagedService>> GetProjectServicesAsync()
+        {
+            var request = Service.Services.List();
+            request.ConsumerId = $"project:{ProjectId}";
+
+            return await LoadPagedListAsync(
+                (token) =>
+                {
+                    request.PageToken = token;
+                    return request.ExecuteAsync();
+                },
+                x => x.Services,
+                x => x.NextPageToken);
+        }
+
+        public async Task EnableServiceAsync(string serviceName)
+        {
+            var operation = await Service.Services
+                .Enable(new EnableServiceRequest { ConsumerId = ProjectId }, serviceName)
+                .ExecuteAsync();
+        }
+    }
+}
