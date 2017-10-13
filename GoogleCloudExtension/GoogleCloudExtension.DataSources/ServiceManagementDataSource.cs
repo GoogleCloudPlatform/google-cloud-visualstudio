0b6cc5823b1979da5e39cebad7a1f847f670436a
Ivan Naranjo <ivann@google.com>
2017-10-13 15:06:19 +0100

    Initial drop of the ApiManager.

--- GoogleCloudExtension/GoogleCloudExtension.DataSources/ServiceManagementDataSource.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/ServiceManagementDataSource.cs
@@ -1,4 +1,5 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿using Google;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.ServiceManagement.v1;
 using Google.Apis.ServiceManagement.v1.Data;
 using System;
@@ -52,6 +53,19 @@ namespace GoogleCloudExtension.DataSources
             return enabledServices.Select(x => x.ServiceName).Contains(serviceName);
         }
 
+        public async Task<Service> GetServiceAsync(string serviceName)
+        {
+            try
+            {
+                var configs = await GetServiceConfigurationsAsync(serviceName);
+                return configs.FirstOrDefault();
+            }
+            catch (GoogleApiException ex)
+            {
+                throw new DataSourceException($"Failed to read service {serviceName}", ex);
+            }
+        }
+
         public async Task EnableServiceAsync(string serviceName)
         {
             var operation = await Service.Services
@@ -63,6 +77,20 @@ namespace GoogleCloudExtension.DataSources
                 refreshOperation: x => Service.Operations.Get(x.Name).ExecuteAsync(),
                 isFinished: x => x.Done ?? false,
                 getErrorData: x => x.Error?.Message);
+        }
+
+        public async Task<IList<Service>> GetServiceConfigurationsAsync(string serviceName)
+        {
+            var request = Service.Services.Configs.List(serviceName);
+
+            return await LoadPagedListAsync(
+                (token) =>
+                {
+                    request.PageToken = token;
+                    return request.ExecuteAsync();
+                },
+                x => x.ServiceConfigs,
+                x => x.NextPageToken);
         }
     }
 }
---
+++ GoogleCloudExtension/GoogleCloudExtension/ApiManagement/ApiManager.cs
@@ -0,0 +1,77 @@
+﻿using GoogleCloudExtension.Accounts;
+using GoogleCloudExtension.DataSources;
+using GoogleCloudExtension.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.ApiManagement
+{
+    public class ApiManager
+    {
+        private static readonly Lazy<ApiManager> s_defaultManager = new Lazy<ApiManager>();
+
+        public static ApiManager Default => s_defaultManager.Value;
+
+        private Lazy<ServiceManagementDataSource> _dataSource = new Lazy<ServiceManagementDataSource>(CreateDataSource);
+
+        private ApiManager()
+        {
+            CredentialsStore.Default.CurrentAccountChanged += OnCurrentCredentialsChanged;
+            CredentialsStore.Default.Reset += OnCurrentCredentialsChanged;
+        }
+
+        public async Task<bool> EnsureServiceEnabledAsync(string serviceName)
+        {
+            var dataSource = _dataSource.Value;
+            if (dataSource == null)
+            {
+                return false;
+            }
+
+            if (await dataSource.IsServiceEnabledAsync(serviceName))
+            {
+                // Nothing to do.
+                Debug.WriteLine($"The service {serviceName} is already enabled.");
+                return true;
+            }
+
+            // Need to enable the service.
+            Debug.WriteLine($"Need to enable the service {serviceName}.");
+            var service = await dataSource.GetServiceAsync(serviceName);
+            if (!UserPromptUtils.ActionPrompt(
+                    prompt: $"Do you want to enable the API {service.Title}?",
+                    title: "Enable needed API",
+                    actionCaption: "Enable"))
+            {
+                return false;
+            }
+
+            await dataSource.EnableServiceAsync(serviceName);
+            return true;
+        }
+
+        private void OnCurrentCredentialsChanged(object sender, EventArgs e)
+        {
+            _dataSource = new Lazy<ServiceManagementDataSource>(CreateDataSource);
+        }
+
+        private static ServiceManagementDataSource CreateDataSource()
+        {
+            if (CredentialsStore.Default.CurrentProjectId != null)
+            {
+                return new ServiceManagementDataSource(
+                    CredentialsStore.Default.CurrentProjectId,
+                    CredentialsStore.Default.CurrentGoogleCredential,
+                    GoogleCloudExtensionPackage.VersionedApplicationName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
