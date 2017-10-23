453860c916a8d705da8f1ea36ceb07bd8bc0e25a
Ivan Naranjo <ivann@google.com>
2017-10-23 16:36:38 +0100

    Adding extensions to get whether a location supports flexible or standard.

---
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/GaeLocationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Google.Apis.Appengine.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.DataSources
+{
+    /// <summary>
+    /// This class containes extension useable for <seealso cref="Location"/> instances.
+    /// </summary>
+    public static class GaeLocationExtensions
+    {
+        public static bool IsFlexEnabled(this Location self)
+        {
+            object value;
+            if (!self.Metadata.TryGetValue("flexibleEnvironmentAvailable", out value))
+            {
+                return false;
+            }
+
+            return (bool)value;
+        }
+
+        public static bool IsStandardEnabled(this Location self)
+        {
+            object value;
+            if (!self.Metadata.TryGetValue("flexibleEnvironmentAvailable", out value))
+            {
+                return false;
+            }
+
+            return (bool)value;
+        }
+    }
+}
