79454fbc13e704b01d1b321be5cfb779e0c2cc25
Ivan Naranjo <ivann@google.com>
2017-10-23 16:59:05 +0100

    Initial drop of dialog.

---
+++ GoogleCloudExtension/GoogleCloudExtension/AppEngineManagement/AppEngineManagementViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GoogleCloudExtension.Accounts;
+using GoogleCloudExtension.DataSources;
+using GoogleCloudExtension.Utils;
+using GoogleCloudExtension.Utils.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.AppEngineManagement
+{
+    public class AppEngineManagementViewModel : ViewModelBase
+    {
+        private readonly AppEngineManagementWindow _owner;
+        private string _selectedLocation;
+
+        public string SelectedLocation
+        {
+            get { return _selectedLocation; }
+            set { SetValueAndRaise(ref _selectedLocation, value); }
+        }
+
+        public AsyncProperty<IEnumerable<string>> Locations { get; }
+
+        public AppEngineManagementViewModel(AppEngineManagementWindow owner)
+        {
+            _owner = owner;
+
+            Locations = new AsyncProperty<IEnumerable<string>>(ListAllLocationsAsync());
+        }
+
+        private async Task<IEnumerable<string>> ListAllLocationsAsync()
+        {
+            var source = new GaeDataSource(
+                CredentialsStore.Default.CurrentProjectId,
+                CredentialsStore.Default.CurrentGoogleCredential,
+                GoogleCloudExtensionPackage.VersionedApplicationName);
+            var possibleLocations = await source.GetAvailableLocationsAsync();
+
+            return possibleLocations.Where(x => x.IsFlexEnabled()).Select(x => x.LocationId);
+        }
+    }
+}
---
+++ GoogleCloudExtension/GoogleCloudExtension/AppEngineManagement/AppEngineManagementWindow.cs
@@ -0,0 +1,28 @@
+﻿using GoogleCloudExtension.Theming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudExtension.AppEngineManagement
+{
+    public class AppEngineManagementWindow : CommonDialogWindowBase
+    {
+        private AppEngineManagementViewModel ViewModel { get; }
+
+        private AppEngineManagementWindow()
+            : base("App Engine App")
+        {
+            ViewModel = new AppEngineManagementViewModel(this);
+            Content = new AppEngineManagementWindowContent { DataContext = ViewModel };
+        }
+
+        public static string PromptUser()
+        {
+            var dialog = new AppEngineManagementWindow();
+            dialog.ShowModal();
+            return dialog.ViewModel.SelectedLocation;
+        }
+    }
+}
