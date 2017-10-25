f1fb4999f7c6876e03560270edf3c254058399ea
Ivan Naranjo <ivann@google.com>
2017-10-25 14:56:36 +0100

    Sorting/organizing usings.

--- GoogleCloudExtension/GoogleCloudExtension/AppEngineManagement/AppEngineManagementViewModel.cs
+++ GoogleCloudExtension/GoogleCloudExtension/AppEngineManagement/AppEngineManagementViewModel.cs
@@ -2,10 +2,8 @@
 using GoogleCloudExtension.DataSources;
 using GoogleCloudExtension.Utils;
 using GoogleCloudExtension.Utils.Async;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
