73832f63944da5653a8e92214b00a51b47c1e081
Ivan Naranjo <ivann@google.com>
2017-11-15 14:25:07 +0000

    Organizing usings.

--- GoogleCloudExtension/GoogleCloudExtension.DataSources/IGaeDataSource.cs
+++ GoogleCloudExtension/GoogleCloudExtension.DataSources/IGaeDataSource.cs
@@ -13,10 +13,7 @@
 // limitations under the License.
 
 using Google.Apis.Appengine.v1.Data;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GoogleCloudExtension.DataSources
