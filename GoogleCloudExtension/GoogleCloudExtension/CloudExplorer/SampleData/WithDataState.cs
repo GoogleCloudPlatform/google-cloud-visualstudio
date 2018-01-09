69c30ca208c51e4901dcc55c9cd0331934ad68f6
Ivan Naranjo <ivann@google.com>
2018-01-09 17:18:13 +0000

    Avoid the string detector.

--- GoogleCloudExtension/GoogleCloudExtension/CloudExplorer/SampleData/WithDataState.cs
+++ GoogleCloudExtension/GoogleCloudExtension/CloudExplorer/SampleData/WithDataState.cs
@@ -23,6 +23,8 @@ namespace GoogleCloudExtension.CloudExplorer.SampleData
 {
     /// <summary>
     /// Mocked version of <seealso cref="CloudExplorerViewModel"/> that has same sample data to show.
+    /// Note: the pattern ("string") is used in this file to make sure that these strings don't trigger the
+    /// detector of strings in need of localization.
     /// </summary>
     public class WithDataState : SampleDataBase
     {
@@ -30,37 +32,37 @@ namespace GoogleCloudExtension.CloudExplorer.SampleData
 
         public AsyncProperty<string> ProfileNameAsync { get; } = new AsyncProperty<string>("User Name");
 
-        public string ProjectDisplayString { get; } = "Project-Id";
+        public string ProjectDisplayString { get; } = ("Project-Id");
 
         public IList<TreeHierarchy> Roots { get; } = new List<TreeHierarchy>
         {
             new TreeHierarchy(new List<TreeNode>
             {
-                new TreeLeaf { Caption = "Node 1", Icon = s_instanceIcon.Value },
-                new TreeLeaf { Caption = "Node 2", Icon = s_instanceIcon.Value }
+                new TreeLeaf { Caption = ("Node 1"), Icon = s_instanceIcon.Value },
+                new TreeLeaf { Caption = ("Node 2"), Icon = s_instanceIcon.Value }
             })
             {
-                Caption = "Container 1",
+                Caption = ("Container 1"),
                 Icon = s_containerIcon.Value,
                 IsExpanded = true,
             },
             new TreeHierarchy(new List<TreeNode>
             {
-                new TreeLeaf { Caption = "Node1", Icon = s_instanceIcon.Value }
+                new TreeLeaf { Caption = ("Node1"), Icon = s_instanceIcon.Value }
             })
             {
-                Caption = "Container 2",
+                Caption = ("Container 2"),
                 Icon = s_containerIcon.Value,
             },
             new TreeHierarchy(new List<TreeNode>
             {
-                new TreeLeaf { Caption = "Warning", IsWarning = true },
-                new TreeLeaf { Caption = "Error", IsError = true },
-                new TreeLeaf { Caption = "Loading", IsLoading = true }
+                new TreeLeaf { Caption = ("Warning"), IsWarning = true },
+                new TreeLeaf { Caption = ("Error"), IsError = true },
+                new TreeLeaf { Caption = ("Loading"), IsLoading = true }
 
             })
             {
-                Caption = "Variants",
+                Caption = ("Variants"),
                 Icon = s_containerIcon.Value,
                 IsExpanded = true
             }
