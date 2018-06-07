fd135e8e8db6383038d22838661aad9bd6973fc2
Jim Przybylinski <JimWP@google.com>
2018-06-07 14:38:37 -0700

    Add tests for GcpPublishStepUtils.ValidateServiceName.

--- GoogleCloudExtension/GoogleCloudExtensionUnitTests/Utils/Validation/StringValidationResultTests.cs
+++ GoogleCloudExtension/GoogleCloudExtensionUnitTests/Utils/Validation/StringValidationResultTests.cs
@@ -16,53 +16,104 @@ using GoogleCloudExtension;
 using GoogleCloudExtension.Utils.Validation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Windows.Controls;
 
 namespace GoogleCloudExtensionUnitTests.Utils.Validation
 {
     [TestClass]
     public class StringValidationResultTests
     {
+        private const string FieldName = "Field name";
+        private const int MaxChars = 10;
+
         [TestMethod]
         public void ValidateExceptionOnMissingResource()
         {
             const string resourceName = "not exists";
-            try
-            {
-                StringValidationResult.FromResource(resourceName);
-                Assert.Fail();
-            }
-            catch (ArgumentException e)
-            {
-                Assert.IsInstanceOfType(e, typeof(ArgumentException));
+            string expectedMessage =
+                string.Format(Resources.Culture, Resources.ExceptionResourceNotFoundMessage, resourceName);
 
-                string expectedMessage =
-                    String.Format(Resources.Culture, Resources.ExceptionResourceNotFoundMessage, resourceName);
-                Assert.IsTrue(e.Message.StartsWith(expectedMessage));
+            var e = Assert.ThrowsException<ArgumentException>(
+                () => StringValidationResult.FromResource(resourceName), expectedMessage);
 
-                MethodInfo targetMethod =
-                    typeof(StringValidationResult).GetMethod(
-                        nameof(StringValidationResult.FromResource), new[] { typeof(string), typeof(object[]) });
-                string expectedParameterName =
-                    targetMethod.GetParameters().Single(pi => pi.ParameterType == typeof(string)).Name;
-                Assert.AreEqual(expectedParameterName, e.ParamName);
-            }
+            MethodInfo targetMethod =
+                typeof(StringValidationResult).GetMethod(
+                    nameof(StringValidationResult.FromResource), new[] { typeof(string), typeof(object[]) });
+            Debug.Assert(targetMethod != null);
+            string expectedParameterName =
+                targetMethod.GetParameters().Single(pi => pi.ParameterType == typeof(string)).Name;
+            Assert.AreEqual(expectedParameterName, e.ParamName);
         }
 
         [TestMethod]
         public void Success()
         {
-            const string fieldName = "Field name";
-            string expectedMessage = String.Format(
-                Resources.Culture, Resources.ValidationMaxCharactersMessage, fieldName, 10);
-            var result = StringValidationResult.FromResource(
-                nameof(Resources.ValidationMaxCharactersMessage), fieldName, 10);
+            string expectedMessage = string.Format(
+                Resources.Culture, Resources.ValidationMaxCharactersMessage, FieldName, MaxChars);
+            StringValidationResult result = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
 
             Assert.AreEqual(expectedMessage, result.Message);
             Assert.AreEqual(expectedMessage, result.ErrorContent);
             Assert.AreEqual(expectedMessage, result.ToString());
             Assert.IsFalse(result.IsValid);
+        }
+
+        public static IEnumerable<object> NotEqualObjects { get; } = new[]
+        {
+            new object[] {null},
+            new object[] {new object()},
+            new object[] {ValidationResult.ValidResult},
+            new object[]
+            {
+                StringValidationResult.FromResource(nameof(Resources.ValidationStartLetterOrNumberMessage), FieldName)
+            }
+        };
+
+        [TestMethod]
+        [DynamicData(nameof(NotEqualObjects))]
+        public void TestEquals_False(object notEqualObject)
+        {
+            StringValidationResult result = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
+
+            Assert.IsFalse(result.Equals(notEqualObject));
+        }
+
+        [TestMethod]
+        public void TestEqual_True()
+        {
+            StringValidationResult result1 = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
+            StringValidationResult result2 = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
+
+            // ReSharper disable once EqualExpressionComparison
+            Assert.IsTrue(result1.Equals(result1));
+            Assert.IsTrue(result1.Equals(result2));
+        }
+
+        [TestMethod]
+        public void TestHashCode()
+        {
+            StringValidationResult result = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
+            StringValidationResult equalResult = StringValidationResult.FromResource(
+                nameof(Resources.ValidationMaxCharactersMessage), FieldName, MaxChars);
+
+            StringValidationResult notEqualResult = StringValidationResult.FromResource(
+                nameof(Resources.ValidationStartLetterOrNumberMessage), FieldName);
+
+            Assert.AreNotEqual(0, result.GetHashCode());
+            Assert.AreNotEqual(ValidationResult.ValidResult.GetHashCode(), result.GetHashCode());
+            Assert.AreNotEqual(notEqualResult.GetHashCode(), result.GetHashCode());
+
+            Assert.AreEqual(result.GetHashCode(), result.GetHashCode());
+            Assert.AreEqual(equalResult.GetHashCode(), result.GetHashCode());
         }
     }
 }
